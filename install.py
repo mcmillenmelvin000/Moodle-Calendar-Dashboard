37ebf75355ded2353bfd72a62bbff58b2d60bb33
mcmillenmelvin000 <mcmillen.melvin@ic.edu>
2026-01-29 12:22:11 -0600

    Initial commit with install.py

---
+++ install.py
@@ -0,0 +1,19 @@
+# moodle-calendar-dashboard
+
+This is the install script for setting up and executing the Moodle Calendar Dashboard.
+
+## Installation
+
+To install the required packages, run:
+
+```
+pip install -r requirements.txt
+```
+
+## Execution
+
+To run the dashboard, use:
+
+```
+python dashboard.py
+```
